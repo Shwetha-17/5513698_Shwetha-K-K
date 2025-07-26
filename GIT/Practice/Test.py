3b53a20dba11dcad3ba0146a386fb37ebf2916b5
Shwetha K K <shwethakk55@gmail.com>
2025-07-26 21:58:06 +0530

    Merge conflict resolved using Rebase

--- GIT/Practice/Test.py
+++ GIT/Practice/Test.py
@@ -1,26 +1,10 @@
 a=5 
 b=5
 c=10
-<<<<<<< HEAD
-<<<<<<< HEAD
 p=a+b+c #addition
 q=a-b #subtraction
-r=b*c #division
-s=b/c #multiplication
-
-
-
-=======
-sum=a+b+c
-diff=a-b
-e=c*a
-d=b/a
->>>>>>> 0ec21dd (Made changes in the same line as in feature branch to create conflict between the branches)
-=======
-sum=a+b+c #addition 
-diff=a-b  #subtraction
-d=b*c     #multiplication
-e=b/c     #division
+z=b*c #multiply
+s=b/c #divide
 
 if a>c:
     print("a is greater")
@@ -28,6 +12,4 @@ else:
     print("c is greater")
 
 
-
->>>>>>> 67f8c86 (Made changes in this file by using Stash Pop)
 
