b8e51c3c87f16384639e6b0ae22a42fcf710439c
Shwetha K K <shwethakk55@gmail.com>
2025-07-26 20:59:26 +0530

    Made changes in this file by using Stash Pop

--- GIT/Practice/Test.py
+++ GIT/Practice/Test.py
@@ -2,6 +2,7 @@ a=5
 b=5
 c=10
 <<<<<<< HEAD
+<<<<<<< HEAD
 p=a+b+c #addition
 q=a-b #subtraction
 r=b*c #division
@@ -15,4 +16,18 @@ diff=a-b
 e=c*a
 d=b/a
 >>>>>>> 0ec21dd (Made changes in the same line as in feature branch to create conflict between the branches)
+=======
+sum=a+b+c #addition 
+diff=a-b  #subtraction
+d=b*c     #multiplication
+e=b/c     #division
+
+if a>c:
+    print("a is greater")
+else:
+    print("c is greater")
+
+
+
+>>>>>>> 67f8c86 (Made changes in this file by using Stash Pop)
 
