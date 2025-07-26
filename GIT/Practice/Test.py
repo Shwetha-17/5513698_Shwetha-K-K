8ca09fe23453b39bbe240f3bb85f9c63195b0d04
Shwetha K K <shwethakk55@gmail.com>
2025-07-26 19:37:41 +0530

    Made changes in the same line as in feature branch to create conflict between the branches

--- GIT/Practice/Test.py
+++ GIT/Practice/Test.py
@@ -1,6 +1,7 @@
 a=5 
 b=5
 c=10
+<<<<<<< HEAD
 p=a+b+c #addition
 q=a-b #subtraction
 r=b*c #division
@@ -8,4 +9,10 @@ s=b/c #multiplication
 
 
 
+=======
+sum=a+b+c
+diff=a-b
+e=c*a
+d=b/a
+>>>>>>> 0ec21dd (Made changes in the same line as in feature branch to create conflict between the branches)
 
