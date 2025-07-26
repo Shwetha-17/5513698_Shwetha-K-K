f7baf7eb7113512071c6a4fe09cf8560e89059a5
Shwetha K K <shwethakk55@gmail.com>
2025-07-26 20:28:44 +0530

    Created stash command in Feature_2 branch and switched to feature branch

--- GIT/Practice/Test.py
+++ GIT/Practice/Test.py
@@ -1,9 +1,11 @@
 a=5 
 b=5
 c=10
-sum=a+b+c #addition
-diff=a-b #subtraction
-d=b*c #division
-e=b/c #multiplication
+p=a+b+c #addition
+q=a-b #subtraction
+r=b*c #division
+s=b/c #multiplication
+
+
 
 
