67f8c868ae803760231dc42b1d664904e865a573
Shwetha K K <shwethakk55@gmail.com>
2025-07-26 20:59:26 +0530

    Made changes in this file by using Stash Pop

--- GIT/Practice/Test.py
+++ GIT/Practice/Test.py
@@ -1,9 +1,16 @@
 a=5 
 b=5
 c=10
-sum=a+b+c
-diff=a-b
-d=b*c
-e=b/c
+sum=a+b+c #addition 
+diff=a-b  #subtraction
+d=b*c     #multiplication
+e=b/c     #division
+
+if a>c:
+    print("a is greater")
+else:
+    print("c is greater")
+
+
 
 
